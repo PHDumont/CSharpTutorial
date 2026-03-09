1d32b84dfef7c47161231bc8683830e195c7580b
Pedro Henrique Dumont <pedro.dumont@dinfor.uniceub>
2026-03-09 08:48:59 -0300

    Mudando o 004-Operators para portugues

--- 004-Operators/Operators/Program.cs
+++ 004-Operators/Operators/Program.cs
@@ -9,70 +9,70 @@ namespace Program
            int Num1, Num2, result;
            char option;
 
-           Console.Write("Enter the First Number: ");
+           Console.Write("Escreva o primeiro numero: ");
            Num1 = Convert.ToInt32(Console.ReadLine());
-           Console.Write("Enter the Second Number: ");
+           Console.Write("Escreva o segundo numero: ");
            Num2 = Convert.ToInt32(Console.ReadLine());
 
-           Console.WriteLine("Main Menu");
-           Console.WriteLine("1. Addition");
-           Console.WriteLine("2. Subtraction");
-           Console.WriteLine("3. Multiplication");
-           Console.WriteLine("4. Division");
-           Console.WriteLine("5. Modulus (remainder of division)");
-           Console.WriteLine("6. Increment and Decrement");
-           Console.WriteLine("7. Logical Operators");
-           Console.WriteLine("8. Comparison Operators");
-           Console.Write("Enter the Operation you choose to perform: ");
+           Console.WriteLine("Tabela");
+           Console.WriteLine("1. Adição");
+           Console.WriteLine("2. Subtração");
+           Console.WriteLine("3. Multiplicação");
+           Console.WriteLine("4. Divisão");
+           Console.WriteLine("5. Resto da divisão");
+           Console.WriteLine("6. Incremento e decremento");
+           Console.WriteLine("7. Operadores logicos");
+           Console.WriteLine("8. Operadors de comparação");
+           Console.Write("Escolha o operador: ");
            option = Convert.ToChar(Console.ReadLine());
 
-           switch (option)
-           {
-               case '1':
-                   result = Num1 + Num2;
-                   Console.WriteLine("The result of Addition is: {0}", result);
-                   break;
-               case '2':
-                   result = Num1 - Num2;
-                   Console.WriteLine("The result of Subtraction is: {0}", result);
-                   break;
-               case '3':
-                   result = Num1 * Num2;
-                   Console.WriteLine("The result of Multiplication is: {0}", result);
-                   break;
-               case '4':
-                   if (Num2 == 0)
-                   {
-                       Console.WriteLine("Error: Division by zero");
-                   }
-                   else
-                   {
-                       result = Num1 / Num2;
-                       Console.WriteLine("The result of Division is: {0}", result);
-                   }
-                   break;
-               case '5':
-                   result = Num1 % Num2;
-                   Console.WriteLine("The remainder of Division (Modulus) is: {0}", result);
-                   break;
-               case '6':
-                   Console.WriteLine("Incrementing Num1 by 1: {0}", ++Num1);  // Pre-increment
-                   Console.WriteLine("Decrementing Num2 by 1: {0}", Num2--);  // Post-decrement
-                   break;
-               case '7':
-                   bool isEqual = Num1 == Num2;
-                   Console.WriteLine("Are the numbers equal? {0}", isEqual);
-                   bool isNotGreater = !(Num1 > Num2);
-                   Console.WriteLine("Is Num1 NOT greater than Num2? {0}", isNotGreater);
-                   break;
-               case '8':
-                   Console.WriteLine("Is Num1 greater than Num2? {0}", Num1 > Num2);
-                   Console.WriteLine("Is Num1 less than or equal to Num2? {0}", Num1 <= Num2);
-                   break;
-               default:
-                   Console.WriteLine("Invalid Option");
-                   break;
-           }
+            switch (option)
+            {
+                case '1':
+                    result = Num1 + Num2;
+                    Console.WriteLine("O resultado da Adição é: {0}", result);
+                    break;
+                case '2':
+                    result = Num1 - Num2;
+                    Console.WriteLine("O resultado da Subtração é: {0}", result);
+                    break;
+                case '3':
+                    result = Num1 * Num2;
+                    Console.WriteLine("O resultado da Multiplicação é: {0}", result);
+                    break;
+                case '4':
+                    if (Num2 == 0)
+                    {
+                        Console.WriteLine("Erro: Divisão por zero");
+                    }
+                    else
+                    {
+                        result = Num1 / Num2;
+                        Console.WriteLine("O resultado da Divisão é: {0}", result);
+                    }
+                    break;
+                case '5':
+                    result = Num1 % Num2;
+                    Console.WriteLine("O resto da Divisão (Módulo) é: {0}", result);
+                    break;
+                case '6':
+                    Console.WriteLine("Incrementando Num1 em 1: {0}", ++Num1);  // Pre-increment
+                    Console.WriteLine("Decrementando Num2 em 1: {0}", Num2--);  // Post-decrement
+                    break;
+                case '7':
+                    bool isEqual = Num1 == Num2;
+                    Console.WriteLine("Os números são iguais? {0}", isEqual);
+                    bool isNotGreater = !(Num1 > Num2);
+                    Console.WriteLine("Num1 NÃO é maior que Num2? {0}", isNotGreater);
+                    break;
+                case '8':
+                    Console.WriteLine("Num1 é maior que Num2? {0}", Num1 > Num2);
+                    Console.WriteLine("Num1 é menor ou igual a Num2? {0}", Num1 <= Num2);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
 
            Console.ReadLine();
        }
